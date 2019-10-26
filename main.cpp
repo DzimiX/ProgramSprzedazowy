47d20ae95c13564df89ce8379e1d678f764250ff
DzimiX <DzimiX@users.noreply.github.com>
2019-10-26 20:12:23 +0200

    Adds entryWindow buttons

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #define _UNICODE
 #endif
 
+#define BTN_ENTRY_OPENDB 100
+#define BTN_ENTRY_CREATEDB 101
+#define BTN_ENTRY_SKIPDB 102
+#define BTN_INFO 103
+
 #include <windows.h>
 #include <iostream>
 
@@ -32,13 +37,22 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
     }
 
     HWND entryWindow;
-    entryWindow = CreateWindowEx( WS_EX_CLIENTEDGE, className, TEXT("Program Sprzedażowy"),
+    entryWindow = CreateWindowEx( WS_EX_CLIENTEDGE, className, TEXT("Program Sprzedażowy - wybierz tryb pracy"),
             WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 900, 600, NULL, NULL, hInstance, NULL );
     if( entryWindow == NULL ){
         throw std::runtime_error("Problem z utworzeniem okna entryWindow");
     }
     ShowWindow( entryWindow, nCmdShow );
     UpdateWindow( entryWindow );
+
+    HWND openDB = CreateWindowEx( 0, TEXT("BUTTON"), TEXT("Otwórz bazę danych"),
+            WS_CHILD | WS_VISIBLE, 5, 5, 200, 30, entryWindow, ( HMENU ) BTN_ENTRY_OPENDB, hInstance, NULL );
+    HWND createDB = CreateWindowEx( 0, TEXT("BUTTON"), TEXT("Stwórz nową bazę danych"),
+            WS_CHILD | WS_VISIBLE, 5, 40, 200, 30, entryWindow, ( HMENU ) BTN_ENTRY_CREATEDB, hInstance, NULL );
+    HWND skipDB = CreateWindowEx( 0, TEXT("BUTTON"), TEXT("Pomiń bazę danych"),
+            WS_CHILD | WS_VISIBLE, 5, 75, 200, 30, entryWindow, ( HMENU ) BTN_ENTRY_SKIPDB, hInstance, NULL );
+    HWND info = CreateWindowEx( 0, TEXT("BUTTON"), TEXT("Informacje o programie"),
+            WS_CHILD | WS_VISIBLE, 5, 110, 200, 30, entryWindow, ( HMENU ) BTN_INFO, hInstance, NULL );
 
     while( GetMessage( & mainMessage, NULL, 0, 0 ) ){
         TranslateMessage( & mainMessage );
